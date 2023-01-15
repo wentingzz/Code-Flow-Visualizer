6c75551acdd9c01e487a0de5e6cd73baf1e9d5be
Wenting <wenttzzheng@gmail.com>
2023-01-15 14:04:24 -0800

    Adds BB

--- prev/WrongIF.py
+++ prev/WrongIF.py
@@ -34,3 +34,83 @@ class IR:
         self.op = opcode
         self.val1 = val1
         self.val2 = val2
+class B:
+    def __init__(self, bb_num, dom = None, left = None):
+        self.dom = dom
+        self.var = {}
+        if dom != None:
+            self.var = dict(dom.var)
+        self.irs = []
+        self.num = bb_num
+        self.left = left
+        self.join = None
+        self.loop = -1
+    def __repr__(self):
+        res = f"BB{self.num}| {{"
+        for ir in self.irs:
+            res += f" {ir} |"
+        return res[:-1]+"}}"
+    def setNum(self, num):
+        self.num = num
+    def addToBBs(self):
+        print(f"Check BB Var: {bbs[-2].var}\n{bbs[-1].var}\n{self.var}")
+        if self.num == -1:
+            self.num = len(bbs)
+        bbs.append(self)
+    def phiPush(self):
+        for key in self.var.keys():
+            if self.var[key].op == "phiTBD":
+                self.var[key].op = "phi"
+                if self.join:
+                    self.updatePhi(key, self.var[key])
+    def updatePhi(self, d, e):
+        if not self.join:
+            return
+        # print(f"Phi: {d}, {e}, BB{self.join.num}, {self.join.var}")
+        if self.left:
+            if self.join.var[d].op == "phiTBD":
+                self.join.var[d].val1 = e
+            else:
+                ir = self.join.addIR("phiTBD", e, self.join.var[d])
+                self.join.var[d] = ir
+                self.join.updatePhi(d, ir)
+        else:
+            if self.join.var[d].op == "phiTBD":
+                self.join.var[d].val2 = e
+            else:
+                ir = self.join.addIR("phiTBD", self.join.var[d], e)
+                self.join.var[d] = ir
+                self.join.updatePhi(d, ir)
+        # print(f"After Phi: {d}, {e}, {self.join.var}")
+    def addIR(self, opcode, val1=None, val2=None):
+        if len(self.irs) == 1 and self.irs[0].op == "" and opcode != "const":
+            self.irs[0].set(opcode, val1, val2)
+            return self.irs[0]
+        if opcode.startswith("b"):
+            tmp = IR(opcode, val1, val2)
+            self.irs.append(tmp)
+            return tmp
+        elif opcode.startswith("phi"):
+            tmp = IR(opcode, val1, val2)
+            if self.loop > -1:
+                self.irs.insert(self.loop, tmp)
+                self.loop += 1
+            else:
+                self.irs.append(tmp)
+            return tmp
+        if opcode == "const":
+            for x in bbs[0].irs:
+                if x.op == opcode and x.val1 == val1:
+                    return x
+            else:
+                tmp = IR(opcode, val1, val2)
+                bbs[0].irs.append(tmp)
+                return tmp
+        else:
+            for x in self.irs:
+                if x.op == opcode and x.val1 == val1 and x.val2 == val2:
+                    return x
+            else:
+                tmp = IR(opcode, val1, val2)
+                self.irs.append(tmp)
+                return tmp
