f0fd3e276cae58bffd7eaaa361d261abfc2bef6d
Wenting <wenttzzheng@gmail.com>
2023-03-25 14:04:24 -0700

    Adds array assignment in if example

--- Test.py
+++ Test.py
@@ -153,4 +153,20 @@ class Test:
         #     call OutputNum(x[a])
         # }."""
 
+        # ----------------------------Array assignment in If----------------------
+        # s = """
+        # main
+        # var a, b;
+        # array[4] x;
+        # {
+        #     let a <- call InputNum ();
+        #     let b <- a;
+        #     let x[a] <- b + 2;
+        #     if b<= 2 then
+        #         let x[b] <- x[a] + 2
+        #     else
+        #         let b <- x[b] + 2
+        #     fi;
+        #     call OutputNum(x[a])
+        # }."""
         self.s = s
